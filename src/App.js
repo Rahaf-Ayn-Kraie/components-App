1412240c6c2931bb1cad789a3c6e279e73ee1459
Rahaf Ayn Kraie <rahaf.aynkraie3@gmail.com>
2025-06-06 00:00:55 -0500

    update

--- src/App.js
+++ src/App.js
@@ -4,14 +4,12 @@ import Banner from "./components/Banner";
 import Gallery from "./components/Gallery";
 import Footer from "./components/Footer";
 import "./css/reset.css";
-
+import pho1 from "./img/pho1.jpg";
+import pho2 from "./img/pho2.jpg";
+import pho3 from "./img/pho3.jpg";
+import pho4 from "./img/pho4.jpg";
 function App() {
-  const images = [
-    "https://via.placeholder.com/150",
-    "https://via.placeholder.com/150",
-    "https://via.placeholder.com/150",
-    "https://via.placeholder.com/150"
-  ];
+  const images = [pho1, pho2, pho3, pho4];
 
   return (
     <>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import "../css/Footer.css";
 function Footer() {
   return (
     <footer className="footer">
-      <p>&copy; 2025 Your Name. All rights reserved.</p>
+      <p>&copy; 2025 Rahaf. All rights reserved.</p>
     </footer>
   );
 }
